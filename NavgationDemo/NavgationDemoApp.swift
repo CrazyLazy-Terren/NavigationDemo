8b4e9f3f16d36c951a91961956b38a05b55a1651
CrazyLazy <crazylazy007@gmail.com>
2025-06-23 19:49:07 +0800

    Initial Commit

---
+++ NavgationDemo/NavgationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavgationDemoApp.swift
+//  NavgationDemo
+//
+//  Created by Tianc Lin on 2025/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NavgationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavgationDemoTests/NavgationDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  NavgationDemoTests.swift
+//  NavgationDemoTests
+//
+//  Created by Tianc Lin on 2025/6/23.
+//
+
+import Testing
+
+struct NavgationDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
