c4f124d9edd0cdb87bc2ef7ce48d326adc186760
CrazyLazy <crazylazy007@gmail.com>
2025-06-30 14:27:48 +0800

    format

--- NavigationDemo/RouterModel.swift
+++ NavigationDemo/RouterModel.swift
@@ -6,47 +6,50 @@
 
 import SwiftUI
 
-enum NavPath:  Hashable {
-    case home, user(Int), profile(Int), settings
-    
-    var name: String {
-        switch self {
-        case .home:
-            return "Home"
-        case .user(let id):
-            return "User \(id)"
-        case .profile(let id):
-            return "Profile \(id)"
-        case .settings:
-            return "Settings"
-        }
+enum NavPath: Hashable {
+  case home
+  case user(Int)
+  case profile(Int)
+  case settings
+
+  var name: String {
+    switch self {
+    case .home:
+      return "Home"
+    case .user(let id):
+      return "User \(id)"
+    case .profile(let id):
+      return "Profile \(id)"
+    case .settings:
+      return "Settings"
     }
-        
+  }
+
 }
 
 @MainActor
 final
-class RouterModel: ObservableObject {
-    var horizontalSize: UserInterfaceSizeClass? = nil
-    @Published var currentPath: NavPath? = .home
-    @Published var path: [NavPath] = []
-    
-    func backToRoot() {
-        // Back to silderbar on compact size like iPhone
-        if horizontalSize == .compact {
-            currentPath = nil
-            return
-        }
-        
-        // Back to next root view (user view in this case)
-        if case .user(_) = currentPath, let user =  UserModel.shared.users.first{
-            currentPath = .user(user.id)
-        }else{
-            path = []
-            currentPath = .home
-            
-        }
-            
+  class RouterModel: ObservableObject
+{
+  var horizontalSize: UserInterfaceSizeClass? = nil
+  @Published var currentPath: NavPath? = .home
+  @Published var path: [NavPath] = []
+
+  func backToRoot() {
+    // Back to silderbar on compact size like iPhone
+    if horizontalSize == .compact {
+      currentPath = nil
+      return
     }
+
+    // Back to next root view (user view in this case)
+    if case .user(_) = currentPath, let user = UserModel.shared.users.first {
+      currentPath = .user(user.id)
+    } else {
+      path = []
+      currentPath = .home
+
+    }
+
+  }
 }
-    
--- NavigationDemo/UserModel.swift
+++ NavigationDemo/UserModel.swift
@@ -7,23 +7,24 @@
 import SwiftUI
 
 struct User: Identifiable {
-    let id: Int
+  let id: Int
 }
 
-@Observable final
-class UserModel{
-    static let shared = UserModel()
-    var users: [User] = []
-    var userCount = 0
-    
-      func addUser() {
-          userCount += 1
-          let newUser = User(id: userCount)
-          users.append(newUser)
-      }
-    
-    func removeUser(_ userID:Int) {
-        users.removeAll { $0.id == userID }
-    }
+@Observable
+final
+  class UserModel
+{
+  static let shared = UserModel()
+  var users: [User] = []
+  var userCount = 0
+
+  func addUser() {
+    userCount += 1
+    let newUser = User(id: userCount)
+    users.append(newUser)
+  }
+
+  func removeUser(_ userID: Int) {
+    users.removeAll { $0.id == userID }
+  }
 }
-    
